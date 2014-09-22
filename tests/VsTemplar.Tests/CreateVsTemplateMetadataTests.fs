0c2a97d8af73d120ceb7d693f0211acdc5561b34
artur-s <arturs@iqmetrix.com>
2014-09-22 09:17:01 -0600

    Fixed VsTemplate file creation test data

--- tests/VsTemplar.Tests/CreateVsTemplateMetadataTests.fs
+++ tests/VsTemplar.Tests/CreateVsTemplateMetadataTests.fs
@@ -11,8 +11,8 @@ let ``It should create VsTemplate metadata file based on provided VS project pat
 
     VsTemplate.CreateMetadataVsTemplateMetadata(fun p -> 
         {p with 
-            VsProjFileLocation = @"C:\Projects\Git\temp\MyProject\Src\IQ.Platform.Framework.Common\IQ.Platform.Framework.Common.csproj"
-            //TODO: if only directory path is provided, create file with project name, e.g. IQ.Platform.Framework.Common.vstemplate
+            VsProjFileLocation = @"C:\Projects\Git\temp\MyProject\Src\MyProject.Common\MyProject.Common.csproj"
+            //TODO: if only directory path is provided, create file with project name, e.g. MyProject.Common.vstemplate
             // Target = @"C:\Projects\Git\temp\MyProject\Template" 
             Target = @"C:\Projects\Git\temp\MyProject\Template\MyTemplateNoWizard.vstemplate"
             })
@@ -22,7 +22,7 @@ let ``It should create VsTemplate metadata file based on provided VS project pat
 
     VsTemplate.CreateMetadataVsTemplateMetadata(fun p -> 
         {p with 
-            VsProjFileLocation = @"C:\Projects\Git\temp\MyProject\Src\IQ.Platform.Framework.Common\IQ.Platform.Framework.Common.csproj"
+            VsProjFileLocation = @"C:\Projects\Git\temp\MyProject\Src\MyProject.Common\MyProject.Common.csproj"
             Target = @"C:\Projects\Git\temp\MyProject\Template\MyTemplateWithWizard.vstemplate"
             WizardTemplate = Some { Assembly = AssemblyName "MyProject.Template.Wizard, Version=1.0.0.0, Culture=neutral, PublicKeyToken=c9a76f51a8a9555f"
                                     FullClassName = "MyProject.Template.Wizard.ChildWizard"}
