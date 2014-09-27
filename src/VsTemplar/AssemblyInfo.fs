2d2b98b24477f9737ce7f4065edcfce54ec3d45d
artur-s <arturs@iqmetrix.com>
2014-09-27 15:08:31 -0600

    Set internals visible to test project

--- src/VsTemplar/AssemblyInfo.fs
+++ src/VsTemplar/AssemblyInfo.fs
@@ -1,11 +1,15 @@
 ﻿namespace System
 open System.Reflection
+open System.Runtime.CompilerServices
+
 
 [<assembly: AssemblyTitleAttribute("VsTemplar")>]
 [<assembly: AssemblyProductAttribute("VsTemplar")>]
 [<assembly: AssemblyDescriptionAttribute("Generates *.vstemplate file based on VS project file: *.csproj, *.fsproj, etc.")>]
 [<assembly: AssemblyVersionAttribute("0.0.7")>]
 [<assembly: AssemblyFileVersionAttribute("0.0.7")>]
+[<assembly: InternalsVisibleTo("VsTemplar.Tests")>]
+
 do ()
 
 module internal AssemblyVersionInformation =
