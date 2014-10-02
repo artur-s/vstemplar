918dab7ea0712e73a0c794b4e27fbb18206544e7
artur-s <arturs@iqmetrix.com>
2014-10-02 08:55:09 -0600

    referenced AutoFixture.AutoFoq and AutoFixture.xUnit nugets.

---
+++ tests/VsTemplar.Tests/TestHelpers.fs
@@ -0,0 +1,10 @@
+﻿module TestHelpers
+
+open Ploeh.AutoFixture
+open Ploeh.AutoFixture.Xunit
+open Ploeh.AutoFixture.AutoFoq
+
+
+type AutoFoqDataAttribute() = 
+    inherit AutoDataAttribute(
+        Fixture().Customize(AutoFoqCustomization()))
