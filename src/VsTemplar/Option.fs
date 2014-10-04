efb43390404cf65b63f82d72ff918f69b4f90ed1
artur-s <arturs@iqmetrix.com>
2014-10-04 11:15:32 -0600

    Added Option helpers and refactored a bit

---
+++ src/VsTemplar/Option.fs
@@ -0,0 +1,12 @@
+﻿module Option
+
+
+let Else second = function
+    | Some _ as first -> first
+    | None -> second
+
+let filter p = function
+    | Some v -> if p v then Some v else None
+    | _ -> None
+
+let ofNullable = function | null -> None | i -> Some i
