20525b2d958b35f6b920f9b4995f5cd4312c7e22
artur-s <arturs@iqmetrix.com>
2014-10-18 16:30:49 -0600

    Published new version

--- src/VsTemplar/AssemblyInfo.fs
+++ src/VsTemplar/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("VsTemplar")>]
 [<assembly: AssemblyProductAttribute("VsTemplar")>]
 [<assembly: AssemblyDescriptionAttribute("Generates *.vstemplate file based on VS project file: *.csproj, *.fsproj, etc.")>]
-[<assembly: AssemblyVersionAttribute("0.0.7")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.7")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: InternalsVisibleToAttribute("VsTemplar.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.7"
+    let [<Literal>] Version = "0.1.0"
